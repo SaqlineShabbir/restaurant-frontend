55fcfb96903f5f844021484c943a22fb21aa9be7
SaqlineShabbir <saqline5869@gmail.com>
2024-02-11 15:50:44 +0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/restaurant-sage-theta/', // Adjust this based on your deployment path
+  base: 'https://restaurant-sage-theta.vercel.app/', // Adjust this based on your deployment path
   plugins: [react()],
 });
