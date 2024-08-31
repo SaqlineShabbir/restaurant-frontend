723c7a6eb42058116645104cb1932f737e4388ce
SaqlineShabbir <saqline5869@gmail.com>
2024-08-31 20:18:42 +0600

    changes in footer

--- src/components/menu/MenuCard.jsx
+++ src/components/menu/MenuCard.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MenuCard = ({ menu }) => {
   return (
@@ -9,8 +9,12 @@ const MenuCard = ({ menu }) => {
         search: `?id=${menu._id}`,
       }}
     >
-      <div className="border lg:w-[300px] dark:text-gray-50 overflow-hidden transform hover:scale-105 transition-transform duration-300 ease-in-out">
-        <img className="lg:h-[200px] lg:w-[300px] object-cover" src={menu.photo} alt={menu.name} />
+      <div className="border lg:w-[300px] lg:h-[400px] dark:text-gray-50 overflow-hidden transform hover:scale-105 transition-transform duration-300 ease-in-out">
+        <img
+          className="lg:h-[200px] lg:w-[300px] object-cover"
+          src={menu.photo}
+          alt={menu.name}
+        />
 
         <div className="relative  ">
           <span className="absolute top-2 right-2 font-bold bg-yellow-400 text-white px-3 py-1 rounded text-sm">
@@ -19,8 +23,12 @@ const MenuCard = ({ menu }) => {
         </div>
 
         <div className="space-y-4 py-5 px-6 ">
-          <p className="text-2xl font-bold dark:text-gray-50 text-gray-800">{menu.name}</p>
-          <p className="text-gray-600 dark:text-gray-50 leading-tight">{menu.description.slice(0, 70)}</p>
+          <p className="text-2xl font-bold dark:text-gray-50 text-gray-800">
+            {menu.name}
+          </p>
+          <p className="text-gray-600 dark:text-gray-50 leading-tight">
+            {menu.description.slice(0, 58)}
+          </p>
 
           <button className="block w-full bg-gradient-to-r from-orange-500 to-yellow-500 text-white px-6 py-2  hover:opacity-90 transition duration-300 ease-in-out focus:outline-none focus:ring focus:border-indigo-300">
             <span className="flex items-center justify-center">
@@ -32,14 +40,18 @@ const MenuCard = ({ menu }) => {
                 viewBox="0 0 24 24"
                 className="w-5 h-5 ml-2"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                ></path>
               </svg>
             </span>
           </button>
         </div>
       </div>
     </Link>
-
   );
 };
 
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BsFacebook, BsLinkedin } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import img from '../../assets/workers-photo/job-application.jpg';
+import React from "react";
+import { BsFacebook, BsLinkedin } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import img from "../../assets/logo/Restaurant-logo-removebg-preview.png";
 const Footer = () => {
   return (
     <div className="lg:px-[200px] px-5 dark:bg-[#0a0c1c] dark:text-white">
       <div>
         {/* logo */}
-        <div className="py-3">
-          <img className="h-10" src={img} alt="" />
+        <div className="py-3 ">
+          <img className="h-[60px]" src={img} alt="" />
         </div>
         <div className="lg:flex justify-between   pb-10">
           <ul className="items-center space-y-8 md:flex md:space-x-6 md:space-y-0 dark:text-white">
@@ -48,7 +48,7 @@ const Footer = () => {
             className="py-2 border px-2 w-[300px]"
             placeholder="email"
           />
-          <button className="bg-orange-400 px-5 py-2 rounded-r">
+          <button className="bg-orange-400 px-5 py-2 rounded-r text-white">
             Subscribe
           </button>
         </div>
